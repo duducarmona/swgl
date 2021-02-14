a6359e4663b6516aa69f1927881f50f604c78685
Edu Carmona <dudu.carmona@gmail.com>
2021-02-14 01:08:22 +0100

    Add scroll pagination functionality

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,38 +1,53 @@
 import React, { PureComponent } from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
 import axios from 'axios';
 import CharacterDetail from './CharacterDetail';
 
 class Characters extends PureComponent {
 	state = {
 		characters: [],
+		scrollerRef: React.createRef(),
 	};
 
-	componentDidMount() {
-		axios.get('https://swapi.dev/api/people/?page=1').then(response => {
+	loadMore = page => {
+		axios.get(`https://swapi.dev/api/people/?page=${page}`).then(res => {
 			this.setState({
-				characters: response.data.results
+				characters: this.state.characters.concat(res.data.results),
 			});
 		});
-	}
+	};
 
 	render() {
-		const { characters } = this.state;
+		const { scrollerRef, characters } = this.state;
 
 		return (
-      <div>
-        <h1>Star Wars Galactic League</h1>
-        <ul className="list-no-decoration">
-          {characters.map((character, index) => (
-            <li key={index}>
-              <CharacterDetail character={character}/>
-            </li>
-          ))}
-        </ul>
-      </div>
+			<div style={{ height: '700px', overflow: 'auto' }} ref={scrollerRef}>
+				<div>
+					<h1>Star Wars Galactic League</h1>
+					<ul className="list-no-decoration">
+						<InfiniteScroll
+							pageStart={0}
+							loadMore={this.loadMore}
+							hasMore={characters.length < 82}
+							loader={
+								<div className="loader" key={0}>
+									Loading ...
+								</div>
+							}
+							useWindow={false}
+							getScrollParent={() => scrollerRef.current}
+						>
+							{characters.map((character, index) => (
+								<li key={index}>
+									<CharacterDetail character={character} />
+								</li>
+							))}
+						</InfiniteScroll>
+					</ul>
+				</div>
+			</div>
 		);
 	}
 }
-
-Characters.propTypes = {};
 
 export default Characters;
